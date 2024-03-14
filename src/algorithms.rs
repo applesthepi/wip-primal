a1bd38f9538a8dae0da41eeb568ed56ed9774dd7
Jackson Oliver <applesthepi@gmail.com>
2024-03-14 17:22:02 -0400

    line intersection

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,3 +1,4 @@
+use bevy::prelude::Vec2;
 use crate::TilePositionAbs;
 
 pub fn spiral<F: FnMut(&TilePositionAbs) -> bool>(
@@ -34,4 +35,33 @@ pub fn inside_circle(
 	let dy = center.y - tile_position_abs.y;
 	let distance_squared = dx.pow(2) + dy.pow(2);
 	4 * distance_squared <= (radius * 2 + 1).pow(2)
+}
+
+pub fn line_intersection(
+	a0: Vec2,
+	a1: Vec2,
+	b0: Vec2,
+	b1: Vec2,
+) -> bool {
+	let b = a1 - a0;
+	let d = b1 - b0;
+	let dot_d_perp = (b.x * d.y) - (b.y * d.x);
+	if dot_d_perp == 0.0 {
+		return false;
+	}
+
+	let c = b0 - a0;
+	let t = ((c.x * d.y) - (c.y * d.x)) / dot_d_perp;
+	if t < 0.0 || t > 1.0 {
+		return false;
+	}
+
+	let u = ((c.x * b.y) - (c.y * b.x)) / dot_d_perp;
+	if u < 0.0 || u > 1.0 {
+		return false;
+	}
+
+	// let intersection = a0 + (t * b);
+
+	true
 }
